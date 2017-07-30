ebe174190f65b52b7166d10bb699423756bccafe
Tunakay37 <samakintunde37@gmail.com>
2017-07-30 01:05:30 +0100

    Create Print.java
    Program to print text.

---
+++ Print.java
@@ -0,0 +1,6 @@
+class Print {
+  public static void main(String args[]) {
+    String myVar = "Hello world! This is Dipo's program"; //Here, I stored the text we wanted to print to a variable.
+    System.out.println(myVar); //I used this command to print the value of myVar to the System using the print new line command.
+  }
+}
